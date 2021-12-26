f2218d6e46df19c6c2eedbb97093d0555e9d2a08
theprimone <theprimone@users.noreply.github.com>
2021-12-26 14:14:27 +0000

    deploy: 7c412768768507adfa672aa1a5d2b4551ef659db

---
+++ assets/404.34caee30.js
@@ -0,0 +1,1 @@
+import"./index.034b5350.js";import{B as e}from"./button.7d16f063.js";import{R as s}from"./index.7ed4acbc.js";import{j as t,k as o}from"./react-venders.fd1d2b80.js";const n=()=>t(s,{status:"404",title:"404",subTitle:"Sorry, the page you visited does not exist.",extra:t(e,{type:"primary",onClick:()=>o.push("/"),children:"Back Home"})});export{n as default};
